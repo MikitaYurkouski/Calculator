d0ad6faaf08eda07fd757079b48f252d006cd63c
MikitaYurkouski <109351428+MikitaYurkouski@users.noreply.github.com>
2022-08-31 21:26:36 +0300

    Update 15js.js

--- 15js.js
+++ 15js.js
@@ -1,33 +1,21 @@
-var buttonPlus = document.getElementById("buttonPlus");
-var buttonMinus = document.getElementById("buttonMinus");
-var buttonMultiply = document.getElementById("buttonMultiply");
-var buttonDevide = document.getElementById("buttonDevide");
-
+var operationButtons = document.getElementsByClassName("operation-button");
 
 
 var input1 = document.getElementById("number1");
 var input2 = document.getElementById("number2");
-var input3 = document.getElementById("number3");
-
-function getNumber1(){
-    return Number(input1.value);
-}
-function getNumber2(){
-    return Number(input2.value);
-}
 
 
-
-
-function makeOperation (operationCode){
+function makeOperation (operationCode) {
+    var number1 = Number(input1.value);
+    var number2 = Number(input2.value);
     if(operationCode === "+"){
-        var result = getNumber1() + getNumber2();
+        var result = number1 + number2;
     } else if(operationCode === "-") {
-        var result = getNumber1() - getNumber2();
+        var result = number1 - number2;
     } else if(operationCode === "*") {
-        var result = getNumber1() * getNumber2();
+        var result = number1 * number2;
     } else if(operationCode === "/") {
-        var result = getNumber1() / getNumber2();
+        var result = number1 / number2;
     } else {
         window.alert("operation is unknown");
     }
@@ -36,54 +24,16 @@ function makeOperation (operationCode){
 
 }
 
-function onButtonPlusClick() {
-    //console.log ("onButtonPlussClick");
-
-
-   makeOperation("+");
-}
-function onButtonMinusClick() {
-   //console.log ("onButtonMinusClick");
-   
-   makeOperation("-");
-}
-function onButtonMultiplyClick() {
-    //console.log ("onButtonMultiplyClick");                   
-
-    makeOperation("*");                                          // var number1 = Number(input1.value);
-                                                                   //  var number2 = Number(input2.value);
-
-                                                                    //  var summ = number1 * number2;
-                                                                    //window.alert(summ);
-}
-function onButtonDevideClick() {
-    //console.log ("onButtonDevideClick");
+function onOperationButtonClick(eventObject){
     
-    makeOperation("/");
+    var clickedElement = eventObject.currentTarget;
+    var operation = clickedElement.innerHTML;
+    makeOperation(operation);
 }
 
 
-buttonPlus.addEventListener("click", onButtonPlusClick);
-buttonMinus.addEventListener("click", onButtonMinusClick);
-buttonMultiply.addEventListener("click", onButtonMultiplyClick);
-buttonDevide.addEventListener("click", onButtonDevideClick);
-
-
-
-
-// var buttonSqrt = document.getElementById("buttonSqrt");
-
-// function getNumber3(){
-//     return Number (input3.value);
-// }
-
-// // else if(operationCode === "number3"){
-// //     var result = getNumber3();
-// //    }
-
-// function onButtonSqrtClick(){
-//     //makeOperation(Math.sqrt());
-//    var summ =Math.sqrt();
-//    window.alert(summ);
-// }
-// buttonSqrt.addEventListener("click", onButtonSqrtClick);
+for (var i = 0; i < operationButtons.length; i++) {
+    var button = operationButtons[i]
+    button.addEventListener("click", onOperationButtonClick);
+    
+}
